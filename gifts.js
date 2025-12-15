dbc5e45d9e1a9c5c1728440e9dd9e583357e19c7
Leonel Ibarra <cibarraleonel@gmail.com>
2025-12-15 18:26:00 -0300

    cambio datos b y productos

--- gifts.js
+++ gifts.js
@@ -9,7 +9,7 @@ const gifts = [
     link: "https://www.cotodigital.com.ar/sitios/cdigi/productos/balanza-de-cocina-top-house-ek321105/_/R-00477860-00477860-200"
   },
   {
-    title: "Pava eléctrica",
+    title: "Pava electrica",
     price: 40199,
     img: "https://static.cotodigital3.com.ar/sitios/fotos/large/00502100/00502191.jpg",
     link: "https://www.cotodigital.com.ar/sitios/cdigi/productos/pava-el%C3%A9ctrica-top-house-hhb1795d-1-7-l-vidrio/_/R-00502191-00502191-200"
@@ -39,7 +39,67 @@ const gifts = [
     link: "https://www.mercadolibre.com.ar/secador-de-pelo-aire-frio-calor-2000w-kanji-beauty-color-negro/p/MLA42519614"
   },
   {
-    title: "Platos playos x6",
+    title: "Set toallones y toallas (marrón o gris)",
+    price: 19999,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_750759-MLA96418639609_102025-F.webp",
+    link: "https://www.mercadolibre.com.ar/blanqueria-home-comfort-toalla-y-toallon-chocolate-lisa-unidad-1-1-70-cm/p/MLA53547310"
+  },
+  {
+    title: "Set manopla + repasadores + paño + agarradera (rayado)",
+    price: 15705,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_914055-MLA88010943300_072025-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1510860941-kit-cocina-2-repasadores-pano-agarradera-manopla-_JM"
+  },
+  {
+    title: "Mantel (natural)",
+    price: 19900,
+    img: "https://http2.mlstatic.com/D_Q_NP_684169-MLA72249572198_102023-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1145777132-mantel-ecocuero-texturado-impermeable-lavable-1-x-140-mts-_JM"
+  },
+  {
+    title: "Juego de sábanas tamaño King (arena)",
+    price: 46200,
+    img: "https://http2.mlstatic.com/D_Q_NP_927635-MLA99991272635_112025-F.webp",
+    link: "https://www.mercadolibre.com.ar/juego-de-sabanas-king-premium-hotelera-700h-tacto-suave-color-oxford-platinum-700-hq-blanco-diseno-de-la-tela-lisa/p/MLA49823735"
+  },
+  {
+    title: "Acolchado / duvet King (beige)",
+    price: 50990,
+    img: "https://acdn-us.mitiendanube.com/stores/001/487/825/products/img_1307-50ac7195560272589517655071507826-1024-1024.webp",
+    link: "https://www.gangahome.com.ar/productos/acolchado-4-estaciones-california-king-beige-gh-0012-k-bg-ls4gf/"
+  },
+  {
+    title: "Tabla de planchar",
+    price: 23195,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_755882-MLA83771621030_042025-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1816196586-tabla-de-planchar-para-mesa-con-patas-plegables-_JM"
+  },
+  {
+    title: "Plancha",
+    price: 23626,
+    img: "https://http2.mlstatic.com/D_Q_NP_917300-MLA99981505103_112025-F.webp",
+    link: "https://www.mercadolibre.com.ar/plancha-seca-de-ropa-con-base-de-acero-inoxidable-winco-w-35/p/MLA33388394"
+  },
+  {
+    title: "Cestos para ropa",
+    price: 12023,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_667512-MLA76272749996_052024-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1778201668-cesto-canasto-ropa-sucia-tela-impermeable-plegable-c-manija-_JM"
+  },
+  {
+    title: "Caja organizadora GRIS x3",
+    price: 11400,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_771876-MLA93499715036_102025-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1908499246-set-x-3-canasto-caja-organizador-plastico-simil-rattan-chico-_JM"
+  },
+  {
+    title: "Papeleras/Basureros",
+    price: 11151,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_757196-MLA74311800194_022024-F.webp",
+    link: "https://www.mercadolibre.com.ar/cesto-plastico-residuos-pedal-12lts-star/up/MLAU248275222"
+  },
+  {
+    title: "platos playos",
     price: 22999,
     img: "https://http2.mlstatic.com/D_Q_NP_616758-MLA91424799305_092025-F.webp",
     link: "https://www.mercadolibre.com.ar/set-x6-plato-playo-24cm-vop-vidrio-opal-carol/up/MLAU3046661560"
@@ -99,13 +159,13 @@ const gifts = [
     link: "https://www.mercadolibre.com.ar/bifera-de-aluminio-cantiadherente-26x26cm-color-negro/p/MLA47599503"
   },
   {
-    title: "Set de espátulas y cucharones",
+    title: "Set de espátulas/cucharones (acero inoxidable)",
     price: 22950,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_788797-MLA95398150138_102025-F.webp",
     link: "https://www.mercadolibre.com.ar/set-x5-utensilios-cocina-acero-inoxidable-cucharon-espatula/up/MLAU3444158830"
   },
   {
-    title: "Frascos herméticos x6",
+    title: "Frascos hermeticos  x6",
     price: 24849,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_856144-MLA96307592080_102025-F.webp",
     link: "https://www.mercadolibre.com.ar/x-6-frascos-transparentes-gadnic-plastico-reforzado-blanco/p/MLA61441371"
@@ -121,8 +181,34 @@ const gifts = [
     price: 18990,
     img: "https://acdn-us.mitiendanube.com/stores/001/487/825/products/apc_9694-89dc95164cf390e05417338538623954-1024-1024.webp",
     link: "https://www.gangahome.com.ar/productos/set-x3-tablas-de-madera-gh-518/"
+  },
+  {
+    title: "Set de ensaladeras",
+    price: 36054,
+    img: "https://http2.mlstatic.com/D_Q_NP_778130-MLA80595012477_112024-F.webp",
+    link: "https://www.mercadolibre.com.ar/set-juego-4-bowls-ensaladera-marinex-vidrio-linea-plus/up/MLAU2731799981"
+  },
+  {
+    title: "Tabla para pizza",
+    price: 14860,
+    img: "https://http2.mlstatic.com/D_Q_NP_820381-MLA70535283313_072023-F.webp",
+    link: "https://www.mercadolibre.com.ar/pizzera-premium-35cm-algarrobo-reforzado-tabla-para-pizza/up/MLAU311472277"
+  },
+  {
+    title: "Bandeja para horno",
+    price: 9204,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_858309-MLA85627617506_062025-F.webp",
+    link: "https://www.mercadolibre.com.ar/bandeja-para-horno-fuente-placa-acero-antiadherente-cocina/up/MLAU3169487862"
+  },
+  {
+    title: "Pizzera molde antiadherente x3",
+    price: 18515,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_719666-MLA53901030254_022023-F.webp",
+    link: "https://articulo.mercadolibre.com.ar/MLA-1349432743-pizzera-molde-antiadherente-35cm-x-3-babymovil-_JM"
   }
 ];
+
+
 
 
 
