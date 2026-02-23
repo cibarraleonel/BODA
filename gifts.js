1301fb5d8a129e0ad63036113b1b2936d91d850d
Leonel Ibarra <cibarraleonel@gmail.com>
2026-02-23 02:04:11 -0300

    update

--- gifts.js
+++ gifts.js
@@ -28,19 +28,25 @@ const gifts = [
   },
   {
     title: "Mixer",
-    price: 45771,
+    price: 50789,
     img: "https://http2.mlstatic.com/D_Q_NP_856875-MLA74781978523_022024-F.webp",
     link: "https://www.mercadolibre.com.ar/mixer-liliana-rainbow-mix-ah10-negro/p/MLA18351018"
   },
   {
+    title: "Lavarropas Samsung Inverter 6.5kg",
+    price: 579599,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_831634-MLA74290394631_012024-F.webp",
+    link: "https://www.mercadolibre.com.ar/lavarropas-samsung-ww65a4000ee-inverter-65kg-blanco-1000rpm/up/MLAU241425580"
+  },
+  {
     title: "Licuadora Liliana 1Lt",
-    price: 45400,
-    img: "https://http2.mlstatic.com/D_NQ_NP_2X_855543-MLA100034007339_122025-F.webp",
-    link: "https://www.mercadolibre.com.ar/licuadora-liliana-whitenblack-albw10-capacidad-1-lt-color-blanco/p/MLA28034654"
+    price: 50999,
+    img: "https://static.cotodigital3.com.ar/sitios/fotos/large/00610300/00610324.jpg",
+    link: "https://www.cotodigital.com.ar/sitios/cdigi/productos/licuadora-top-house-l-28-500-w-1-5-l-negro/_/R-00610324-00610324-200%3FDy%3D1&&idSucursal%3D200"
   },
   {
     title: "Cafetera Digital De Filtro",
-    price: 73541,
+    price: 78999,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_746249-MLA99451199974_112025-F.webp",
     link: "https://www.mercadolibre.com.ar/cafetera-digital-de-filtro-automatica-con-timer-ca8182p-atma/p/MLA21239144"
   },
@@ -64,7 +70,7 @@ const gifts = [
   },
   {
     title: "Juego de sábanas tamaño Queen (color arena o gris claro)",
-    price: 42664,
+    price: 38308,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_867579-MLA99506594576_112025-F.webp",
     link: "https://www.mercadolibre.com.ar/jean-cartier-oxford-platinum-700-hq-juego-de-sabanas-queen-oxford-platinum-700-hq-arena-liso-240-cm-unidad-1/p/MLA49097726"
   },
@@ -81,6 +87,18 @@ const gifts = [
     link: "https://www.telas.com.ar/productos/edredon-queen-4-estaciones-beige-gris-claro/"
   },
   {
+    title: "Colchón y Sommier Piero",
+    price: 558892,
+    img: "https://images.fravega.com/f300/cee64279427a776dd465b684aa401fc1.jpg.webp",
+    link: "https://www.fravega.com/p/colchon-y-sommier-dos-plazas-piero-foam-espuma-alta-densidad-190-x-140-cm-22006849/"
+  },
+  {
+    title: "Ventilador Liliana 65W",
+    price: 32249,
+    img: "https://www.megatone.net/images/Articulos/65/205/67/thumb/MKT1167LTA-1.webp",
+    link: "https://www.megatone.net/producto/ventilador-turbo-vtf16p-16-65w-3-velocidades-liliana_MKT1167LTA/"
+  },
+  {
     title: "Sillas En Paraíso Macizo x4",
     price: 508000,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_686774-MLA54982177543_052023-F.webp",
@@ -94,13 +112,13 @@ const gifts = [
   },
   {
     title: "Plancha",
-    price: 24208,
+    price: 29500,
     img: "https://http2.mlstatic.com/D_Q_NP_917300-MLA99981505103_112025-F.webp",
     link: "https://www.mercadolibre.com.ar/plancha-seca-de-ropa-con-base-de-acero-inoxidable-winco-w-35/p/MLA33388394"
   },
   {
     title: "Cestos para ropa (gris)",
-    price: 12990,
+    price: 10800,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_770904-MLA79303516016_092024-F.webp",
     link: "https://articulo.mercadolibre.com.ar/MLA-1145077418-cesto-canasto-ropa-sucia-plegable-impermeable-con-tapa-tela-_JM"
   },
@@ -112,14 +130,14 @@ const gifts = [
   },
   {
     title: "Papeleras/Basureros",
-    price: 11738,
+    price: 10916,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_757196-MLA74311800194_022024-F.webp",
     link: "https://www.mercadolibre.com.ar/cesto-plastico-residuos-pedal-12lts-star/up/MLAU248275222"
   },
   {
     title: "Platos playos x6",
     price: 32997,
-    img: "https://http2.mlstatic.com/D_Q_NP_616758-MLA91424799305_092025-F.webp",
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_812505-MLA98379460228_112025-F.webp",
     link: "https://www.mercadolibre.com.ar/set-x6-plato-playo-gourmet-ala-ancha-24-cm-porcelana-oxford/up/MLAU3560897559"
   },
   {
@@ -148,10 +166,17 @@ const gifts = [
   },
   {
     title: "Vasos x6",
-    price: 25000,
+    price: 22171,
     img: "https://http2.mlstatic.com/D_Q_NP_707475-MLA95599998252_102025-F.webp",
     link: "https://www.mercadolibre.com.ar/set-x-12-vaso-vidrio-rigolleau-copon-gourmet-flint-450-ml-transparente/p/MLA57776307"
   },
+  {
+    title: "Medio canasto Matero de Cuero",
+    price: 49900,
+    img: "https://acdn-us.mitiendanube.com/stores/001/104/123/products/1x1-canasta3x3negra-big-b2fadd74d0d4af99a217404962503183-1024-1024.webp",
+    link: "https://www.cushe.com.ar/productos/medio-canasto-matero-de-cuero-con-sujetador/"
+  }
+  ,
   {
     title: "Jarra hervidor",
     price: 10100,
@@ -172,25 +197,25 @@ const gifts = [
   },
   {
     title: "Bífera antiadherente",
-    price: 28967,
-    img: "https://http2.mlstatic.com/D_NQ_NP_2X_788797-MLA95398150138_102025-F.webp",
-    link: "https://www.mercadolibre.com.ar/bifera-aluminio-hudson-antiadherente-26cm-linea-daily/up/MLAU3092720125"
+    price: 28057,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_916594-MLA98653081053_112025-F.webp",
+    link: "https://www.mercadolibre.com.ar/bifera-hudson-daily-antiadherente-26-cm-2-l/up/MLAU236144105#polycard_client=search-desktop&search_layout=grid&position=4&type=product&tracking_id=a96d0fb7-fd29-40bf-a737-57053d1db86c&wid=MLA1141180299&sid=search"
   },
   {
     title: "Set de espátulas/cucharones (acero inoxidable)",
-    price: 29290,
+    price: 23900,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_629981-MLA92802609928_092025-F.webp",
     link: "https://www.mercadolibre.com.ar/set-x5-utensilios-cocina-acero-inoxidable-cucharon-espatula/up/MLAU3444158830"
   },
   {
     title: "Frascos hermeticos  x6",
-    price: 33799,
+    price: 24699,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_856144-MLA96307592080_102025-F.webp",
     link: "https://www.mercadolibre.com.ar/x-6-frascos-transparentes-gadnic-plastico-reforzado-blanco/p/MLA61441371"
   },
   {
     title: "Especieros",
-    price: 27899,
+    price: 29999,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_836356-MLA81224486733_122024-F.webp",
     link: "https://www.mercadolibre.com.ar/raitot-especiero-negro-16-16/p/MLA44582605"
   },
@@ -202,39 +227,39 @@ const gifts = [
   },
   {
     title: "Set de ensaladeras x4",
-    price: 32970,
+    price: 30662,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_683426-MLA82164758149_012025-F.webp",
     link: "https://articulo.mercadolibre.com.ar/MLA-2004749938-set-juego-de-bowls-vidrio-sempre-_JM"
   },
   {
     title: "Tabla para pizza",
     price: 14860,
-    img: "https://http2.mlstatic.com/D_Q_NP_820381-MLA70535283313_072023-F.webp",
-    link: "https://www.mercadolibre.com.ar/pizzera-premium-35cm-algarrobo-reforzado-tabla-para-pizza/up/MLAU311472277"
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_731413-MLA81070621345_112024-F.webp",
+    link: "https://www.mercadolibre.com.ar/tabla-pizzera-picar-cortar-servir-madera-bambu-33cm-filula-color-bambu-natural-tabla-redonda-diametro-33-cm/p/MLA44258486#polycard_client=search-desktop&search_layout=grid&position=12&type=product&tracking_id=d0f23fc7-8ade-47d1-aeb1-8de223894513&wid=MLA1463485577&sid=search"
   },
   {
     title: "Bandeja para horno",
-    price: 9989,
+    price: 9289,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_858309-MLA85627617506_062025-F.webp",
     link: "https://www.mercadolibre.com.ar/bandeja-para-horno-fuente-placa-acero-antiadherente-cocina/up/MLAU3169487862"
   },
   {
     title: "Pizzera molde antiadherente x3",
-    price: 18515,
+    price: 19490,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_719666-MLA53901030254_022023-F.webp",
     link: "https://articulo.mercadolibre.com.ar/MLA-1349432743-pizzera-molde-antiadherente-35cm-x-3-babymovil-_JM"
   },
   {
     title: "Fuente de vidrio",
-    price: 17311,
+    price: 25354,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_821870-MLA100008250493_122025-F.webp",
     link: "https://www.mercadolibre.com.ar/p/MLA27728340"
   },
   {
     title: "Organizador de ropa x3",
-    price: 33000,
-    img: "https://http2.mlstatic.com/D_NQ_NP_2X_912672-MLA88238171356_072025-F.webp",
-    link: "https://www.mercadolibre.com.ar/canasto-ordenador-organizador-de-ropa-plegable-reforzado-x3u/up/MLAU3301324944"
+    price: 20499,
+    img: "https://http2.mlstatic.com/D_NQ_NP_2X_732401-MLA105996204360_022026-F.webp",
+    link: "https://www.mercadolibre.com.ar/contenedor-de-tela-plegable-organizador-canasto-bazar/up/MLAU3771013831?picker=true&quantity=1"
   }
   ];
 
